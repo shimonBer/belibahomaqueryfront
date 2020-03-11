a0c59129d5d264813fe159b7c86d6782a1082390
shimonBer <shimon.ber@gmail.com>
2020-03-11 14:06:29 +0200

    fix-cors

--- src/services/reportServices.js
+++ src/services/reportServices.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
-import {server_address} from '../util/config';
 
 
 export const reportMaker = async (reportProperties) => {
 
     const response = await axios({
         method: 'get',
-        url: `${server_address}/reports/${reportProperties.reportName}?month=${reportProperties.year}-${reportProperties.month}`, 
+        url: `${process.env.REACT_APP_SERVER_ADDRESS}/reports/${reportProperties.reportName}?month=${reportProperties.year}-${reportProperties.month}`, 
         headers: {
             'Content-Type': 'application/json',
             'x-access-token': localStorage.getItem("query-auth-token")
