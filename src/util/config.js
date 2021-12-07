15633625c6db1383e85dc023c84af42bc7e7b351
shimonBer <shimon.ber@gmail.com>
2021-12-07 18:25:13 +0200

    h

--- src/util/config.js
+++ src/util/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    PROD_SERVER_ADDRESS: 'https://belibahomaquerynode.herokuapp.com',
+    PROD_SERVER_ADDRESS: 'http://belibahomaquerynode.herokuapp.com',
     DEV_SERVER_ADDRESS: 'http://localhost:3000'
 }
