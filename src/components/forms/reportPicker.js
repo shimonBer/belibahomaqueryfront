f8eed08fcbc89336218eceb2826f9d56f3e056a9
shimonBer <shimon.ber@gmail.com>
2023-01-04 10:42:48 +0200

    star

--- src/components/forms/reportPicker.js
+++ src/components/forms/reportPicker.js
@@ -87,6 +87,7 @@ export default function ReportPicker(props) {
         "quaterlyKivunReport",
         "allTrainees",
         "allTutors",
+        "maayanBamidbarYearly"
     ]
 
     // useEffect(() => {
--- src/util/config.js
+++ src/util/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     PROD_SERVER_ADDRESS: 'https://belibahomaquerynode.herokuapp.com',
-    DEV_SERVER_ADDRESS: 'http://localhost:3000'
+    DEV_SERVER_ADDRESS: 'http://localhost:3001'
 }
